230d294ce686530b5c5c466c5b8eafcdc7a6a88e
RaymondOren <raymond59oren@gmail.com>
2023-07-16 23:39:36 +0800

    Modified a few files > modified css/style.css > modified html/index.html > modified javascript/anim.js > added new file todo-list.todo NOTE: Must have "To Do Tasks" extension in VSCode by Sandeep Somavarapu to read the contents of the file

--- javascript/anim.js
+++ javascript/anim.js
@@ -1,3 +1,16 @@
-$("#read-more-btn").click(function () {
+$("#read-more-btn").click(function (e) {
     alert("Button is working");
 });
+
+$(".lang").mouseenter(function (e) { 
+    $(this).addClass('lang-hover');
+});
+
+$(".lang").mouseleave(function (e) { 
+    $(this).removeClass('lang-hover');
+});
+
+$(".lang").click(function(e){
+    alert("Lang is clicked");
+});
+
